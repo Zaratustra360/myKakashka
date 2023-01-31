71545fd22839e84462d472a9e56c2c4612ceedd5
Lev <zaratustra360@gmail.com>
2023-01-31 23:39:43 +0500

    ALO

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,7 +7,7 @@ export const Todo = (props) => {
     return (
               <div>
              <p>{title}</p>
-            <p>{description}sasd</p>
+            <p>{description}sasdsdfasd</p>
          </div>
     )
 
